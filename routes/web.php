2de97cabfdb3c9cde47deb457828ec8b8a59439b
gyarmex <gyarmex@gmail.com>
2025-06-28 07:48:38 +0900

    おかしいget処理コメントアウト

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', function () {
-    return "hello world satou kyousuke!";
-});
+// Route::get('/', function () {
+//     return "hello world satou kyousuke!";
+// });
 
 Route::get('/', function () {
     // 適当な画像URLを配列で渡す（例：storage/public/images/ 以下）
