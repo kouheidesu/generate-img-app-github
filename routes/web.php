286a506d189ee1a49a63b6edff8caff7cec07118
gyarmex <gyarmex@gmail.com>
2025-06-28 08:05:50 +0900

    画像アクセス可能に変更

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     // 適当な画像URLを配列で渡す（例：storage/public/images/ 以下）
     $images = [
-        asset('storage/images/it-engeneer.png'),
-        asset('storage/images/it-engeneer.png'),
-        asset('storage/images/it-engeneer.png'),
+        asset('storage/images/it-engineer.png'),
+        asset('storage/images/it-engineer.png'),
+        asset('storage/images/it-engineer.png'),
     ];
     return view('gallery', compact('images'));
 });
